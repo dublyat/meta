62a11906496d08ae795d674adecae5fdd665aca7
dublyat <whit3.ducc@gmail.com>
2025-08-11 23:03:45 +0700

    Update alexakontol.py

--- alexakontol.py
+++ alexakontol.py
@@ -12,7 +12,23 @@ api_hash = 'aacff9e7028153dce900d87e00adfad1'
 phone = '+6281374050106'
 group_ids = ["@testingzz22","@testingz90"]  
 trigger = ["test1","test2","test3"]
-automsg = ["test4","test5","test6"]
+
+automsg = ["Menurut gue, ini langkah yang mantap buat nge-refresh lini depan MU, apalagi dengan gaya mainnya yang fisikal dan cepat.",
+"Lo pikir dia langsung bisa nyatu sama sistem Reuben Amorim, atau masih perlu adaptasi?",
+"iya gue baca juga. Secara gaya main, sih, Sesko punya atribut super keren tnggi, kuat, lari cepat, plus punya kemampuan aerial yang mumpuni.",
+"Dia sempat jadi top scorer di Leipzig, dan ada catatan dia bikin total 39 gol dari 87 penampilan sejak datang dari Salzburg",
+"tapi, gue juga mikir transfer mahal itu juga bawa beban tinggi ke pundaknya.",
+"Harga segitu, fans pasti expect dia langsung ngegemparin EPL. Luka adaptasi liga Inggris kan bisa bikin pressure makin berat. Dan MU sendiri lagi banyak belanja striker Cunha, Mbeumo, dan sekarang Sesko. Apakah ini jadi duplikat di lini depan? Atau mereka membangun rotasi yang tajam?",
+"Guys, gue cuma mau sorotin satu hal finansialnya. Dari laporan, United sekarang lagi di tengah-musim spending galore, bro over £200m buat three new forwards dan totalnya bisa tembus £245m+",
+"yaa sementara itu, dia juga lagi harus manage kerugian ratusan juta, perencanaan stadion £2miliar, dan PSR Profit‑sustainability rules",
+"Gila. Jadi, investasi ke Sesko bukan cuma soal performa, tapi juga gimana caranya bayar tanpa bikin klub collapse.",
+"ges, pemilihan Sesko ini lebih karena potensinya sebagai striker masa depan, atau karena strategi merchandising & hype belaka?",
+"lu sih selalu bawa-bawa finansial mulu, bikin pusing pala barbie. Tapi gue ngerti sih poin lo, apalagi MU sekarang udah nggak bisa seenaknya belanja tanpa mikir efeknya ke aturan keuangan klub.",
+"Tapi ya, dari sisi fans, kita udah lama nggak punya striker muda yang bener-bener bikin jantungan tiap kali dapet bola di kotak penalti. Sesko tuh potensial banget buat jadi bintang baru, mirip Haaland dari segi fisik dan pergerakan.",
+"Gila men, dia tinggi, larinya cepet, finishingnya dingin. Kalo dikasih waktu dan sistem yang pas, dia bisa jadi striker utama bertahun-tahun ke depan. Bukan cuma buat United, tapi buat Liga Inggris juga.",
+"Lu bayangin aja kombinasi dia sama Garnacho di sisi kiri, Bruno ngumpan di belakang, itu mah horror buat tim lawan.",
+"Gue ngelihat Sesko itu sebagai investasi jangka panjang. Tapi tetep, gue masih khawatir dia bakal keteken ekspektasi terlalu dini.",
+"Fans MU itu kan terkenal nggak sabaran, dikit-dikit dibandingin sama legenda kayak Van Persie, Rooney, bahkan Ronaldo."]
 
 client = TelegramClient('anon', api_id, api_hash)
 
@@ -59,6 +75,7 @@ async def main():
 
 # ====== Entry Point ======
 asyncio.run(main())
+
 
 
 
